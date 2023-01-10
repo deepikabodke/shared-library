08bb963560bdcbeb21e2537f457acc0b83cfcbf5
Deepika <deepika.bodke@kissht.com>
2023-01-10 15:55:25 +0530

    comitting groovy file

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -1,44 +1,80 @@
 import hudson.tasks.test.AbstractTestResultAction
+import hudson.model.Actionable
+
 
 def call(String name = 'User') {
     echo "Welcome, ${name}."
+    return buildNotification()
+}
 
-    /*AbstractTestResultAction testResultAction =  currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+void buildNotification()
+{
+    AbstractTestResultAction testResultAction =  currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
     if (testResultAction != null) {
-        echo "Tests: ${testResultAction.failCount} / ${testResultAction.failureDiffString} failures of ${testResultAction.totalCount}.\n\n"
-    }*/
-     def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-     def summary = ""
+        def totalNumberOfTests = testResultAction.totalCount
+        def failedNumberOfTests = testResultAction.failCount
+        def failedDiff = testResultAction.failureDiffString
+        def skippedNumberOfTests = testResultAction.skipCount
+        def passedNumberOfTests = totalNumberOfTests - failedNumberOfTests - skippedNumberOfTests
+        emailTestReport = "Tests Report:\n Passed: ${passedNumberOfTests}; Failed: ${failedNumberOfTests} ${failedDiff}; Skipped: ${skippedNumberOfTests}  out of ${totalNumberOfTests} "
+    }
+}
 
-     if (testResultAction != null) {
-         def total = testResultAction.getTotalCount()
-         def failed = testResultAction.getFailCount()
-         def skipped = testResultAction.getSkipCount()
-
-         summary = "Test results:\n\t"
-         summary = summary + ("Passed: " + (total - failed - skipped))
-         summary = summary + (", Failed: " + failed)
-         summary = summary + (", Skipped: " + skipped)
-     } else {
-         summary = "No tests found"
-     }
-     return summary
-
-    /*@NonCPS
-    def testStatuses() {
-        def testStatus = ""
+/*ArrayList<LinkedHashMap<String, String>> buildNotificationMessage() {
+    try {
         AbstractTestResultAction testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-        if (testResultAction != null) {
-            def total = testResultAction.totalCount
-            def failed = testResultAction.failCount
-            def skipped = testResultAction.skipCount
-            def passed = total - failed - skipped
-            testStatus = "Test Status:\n  Passed: ${passed}, Failed: ${failed} ${testResultAction.failureDiffString}, Skipped: ${skipped}"
+        def build = currentBuild
 
-            if (failed == 0) {
-                currentBuild.result = 'SUCCESS'
+        long startMillis = currentBuild.getStartTimeInMillis()
+        int durationMillis = build.duration
+        def format = new SimpleDateFormat('dd-MMM-YYYY hh:mm:ss a Z z')
+        String startedAt = format.format(new Date(startMillis))
+
+        long finishMillis = startMillis + durationMillis
+        String completedAt = format.format(new Date(finishMillis))
+
+        // Duration duration = Duration.between(Instant.ofEpochMilli(startMillis), Instant.ofEpochMilli(finishMillis));
+        // readableDuration = duration.toString().substring(2).replaceAll(~'(\\d[HMS])(?!$)', '$1 ').toLowerCase();
+        String readableDuration = build.getDurationString()
+        String took = readableDuration.replace('and counting', '')
+
+        ArrayList<LinkedHashMap<String, String>> facts = [[name: "Started At", template: "${startedAt}"],
+                                                          [name: "Completed At", template: "${completedAt}"],
+                                                          [name: "Took", template: "${took}"]
+        ]
+
+        captureTestDetails(testResultAction, facts)
+        return facts
+    } catch (Exception exception) {
+        echo "Error: Exception while Capturing Test Result metrics. Marking the build as UNSTABLE"
+        exception.printStackTrace()
+        return null
+    }
+}
+
+void captureTestDetails(AbstractTestResultAction testResultAction, ArrayList<LinkedHashMap<String, String>> facts) {
+    if (testResultAction != null) {
+        DecimalFormat df = new DecimalFormat("##.##%")
+        int totalTests = testResultAction.totalCount
+        if (totalTests > 0) {
+            int failed = testResultAction.failCount
+            String failedDiff = testResultAction.failureDiffString
+            int skipped = testResultAction.skipCount
+            int passed = totalTests - failed - skipped
+            String passedPercent = df.format(passed / totalTests)
+            String failedPercent = df.format(failed / totalTests)
+            String skippedPercent = df.format(skipped / totalTests)
+            facts.addAll([[name: "Total Tests", template: "${totalTests}"],
+                          [name: "Passed", template: "${passed}"],
+                          [name: "Passed Percentage", template: "${passedPercent}"],
+                          [name: "Failed", template: "${failed} ${failedDiff}"],
+                          [name: "Failed Percentage", template: "${failedPercent}"]])
+            if (skipped > 0) {
+                facts.addAll([[name: "Skipped", template: "${skipped}"],
+                              [name: "Skipped Percentage", template: "${skippedPercent}"]]);
             }
         }
-        return testStatus
-    }*/
-}
+    } else {
+        facts.add([name: "Test Summary", template: "Test results unavailable"])
+    }
+}*/
