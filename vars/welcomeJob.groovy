c16ca06596b3edd800fc6a08d911afccd4e6e682
Deepika <deepika.bodke@kissht.com>
2023-01-10 15:24:50 +0530

    comitting groovy file

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -1,3 +1,22 @@
 def call(String name = 'User') {
     echo "Welcome, ${name}."
+
+    def getTestSummary = { ->
+     def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+     def summary = ""
+
+     if (testResultAction != null) {
+         def total = testResultAction.getTotalCount()
+         def failed = testResultAction.getFailCount()
+         def skipped = testResultAction.getSkipCount()
+
+         summary = "Test results:\n\t"
+         summary = summary + ("Passed: " + (total - failed - skipped))
+         summary = summary + (", Failed: " + failed)
+         summary = summary + (", Skipped: " + skipped)
+     } else {
+         summary = "No tests found"
+     }
+     return summary
+ }
 }
